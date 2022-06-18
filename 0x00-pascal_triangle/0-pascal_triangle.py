e9babec32b740670e0f4189e1983d9353fb68c76
nandweza <allannandweza@gmail.com>
2022-06-18 11:09:54 +0000

    added task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    """
+    function that returns a lsit of lists of integers.
+    representing pascal's triangle of n.
+    """
+
+    arr = [[] for i in range(n)]
+    if n <= 0:
+        return []
+
+    for i in range(n):
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                arr[i].append(1)
+            else:
+                arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
+    return arr
