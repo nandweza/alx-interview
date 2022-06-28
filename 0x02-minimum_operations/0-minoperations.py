fce6ec8bb2c018816da06975048b3ed5a48a8a77
nandweza <allannandweza@gmail.com>
2022-06-28 15:27:02 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations needed
+    to result in exactly n H characters in the file."""
+    if (n < 2):
+        return 0
+
+    Ops, i = 0, 2
+
+    while i <= n:
+        if n % i == 0:
+            Ops += i
+            n = n / i
+            i -= 1
+        i += 1
+    return Ops
