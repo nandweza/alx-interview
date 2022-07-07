783d6bc88b8db3e834d637b93cdc3dd29de302e1
nandweza <allannandweza@gmail.com>
2022-07-07 16:31:42 +0300

    fixed pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,10 +7,12 @@ filesize, count = 0, 0
 codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 stats = {k: 0 for k in codes}
 
+
 def print_stats(stats: dict, file_size: int) -> None:
     print("File size: {:d}".format(filesize))
     for k, v in sorted(stats.items()):
         print("{}: {}".format(k, v))
+
 
 try:
     for line in sys.stdin:
