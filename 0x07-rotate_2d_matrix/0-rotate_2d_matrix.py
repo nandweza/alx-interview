e3b185c58afabeaa878e1042426a4f0e8bec99b6
nandweza <allannandweza@gmail.com>
2022-08-04 18:28:43 +0300

    updates

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,15 +11,18 @@ def rotate_2d_matrix(matrix):
         - You can assume the matrix will have 2 dimensions and will
           not be empty.
     """
-    rows = len(matrix)
-    cols = len(matrix[0])
-    res = []
+    n = len(matrix)
 
-    for i in range(cols):
-        temp = []
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
 
-        for j in range(rows):
-            temp.append(matrix[j][i])
-        res.append(temp[::-1])
-
-    return res
+    # Swap columns
+    for i in range(n):
+        for j in range(n // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n - j - 1]
+            matrix[i][n - j - 1] = temp
