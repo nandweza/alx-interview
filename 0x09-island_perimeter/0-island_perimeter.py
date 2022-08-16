994249b2d192e83fc845960fcc7d9cb5a3f5043e
nandweza <allannandweza@gmail.com>
2022-08-16 18:53:29 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Island Perimeter"""
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid."""
+    count = 0
+    row = len(grid)
+    col = len(grid[0]) if row else 0
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+            check = [1 if k[0] in range(row) and k[1] in range(col)
+                     else 0 for k in idx]
+            
+            if grid[i][j]:
+                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                              for r, k in zip(check, idx)])
+                
+    return (count)
